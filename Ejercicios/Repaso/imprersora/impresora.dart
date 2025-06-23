13e038575ebce3ac47fc4a501f65643ab29f27c7
ALEK21DS <brandonalexpesantez@gmail.com>
2025-06-23 14:07:15 -0500

    RepasoSingleton

---
+++ Ejercicios/Repaso/imprersora/impresora.dart
@@ -0,0 +1,26 @@
+class ImpresoraCentral {
+
+  static final ImpresoraCentral _instancia = ImpresoraCentral._interno();
+  factory ImpresoraCentral() => _instancia;
+  ImpresoraCentral._interno();
+  bool _conectado = false;
+  bool get estaConectada => _conectado;
+
+  void conectar () {
+    if (!_conectado){
+      _conectado = true;
+      print('Impresora conectada correctamente');
+    } else {
+      print('Ya existe una conexion activa');
+    }
+  }
+
+  void imprimir (String archivo) {
+    if(_conectado) {
+      print('IMPRIMIENDO ARCHIVP ${archivo}');
+    } else {
+      print('La impresora no esta conectada');
+    }
+  }
+
+}
